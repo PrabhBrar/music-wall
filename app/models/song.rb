963bce6be1095febd41553c544e7456f62a06fad
Prabhsharan Singh Brar <prabh182006@gmail.com>
2016-02-23 01:11:59 +0000

    version 1

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,6 +2,6 @@ class Song < ActiveRecord::Base
 
   validates :song_title, presence: true
   validates :author, presence: true
-  validates :song_url, format: { with: /(https?:\/\/(?:www\.|(?!www))[^\s\.]+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,})/, message: "valid url's only" }, if: :song_url
+  validates :song_url, allow_blank: true, format: { with: /(https?:\/\/(?:www\.|(?!www))[^\s\.]+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,})/, message: "valid url's only" }
 
 end
